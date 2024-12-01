42ffb55877e421237a3bc4202cfacf95dfaebe2d
Sevban Bayrak <svbnbyrk@gmail.com>
2024-12-01 12:54:21 +0300

    Add mongo external mongo package

---
+++ pkg/db/mongo/mongo.go
@@ -0,0 +1,25 @@
+package mongo
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Mongo struct {
+	db *mongo.Database
+}
+
+func NewMongo(uri, dbName string, timeout time.Duration) (*Mongo, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	clientOpts := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(ctx, clientOpts)
+	if err != nil {
+		return nil, err
+	}
+	return &Mongo{client.Database(dbName)}, nil
+}
